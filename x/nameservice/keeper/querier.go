3f6c973e3d5b69498bb42f5fa2b9ea92e1eee178
Ilia Bukalov <ilyabukalov@gmail.com>
2020-09-30 17:07:41 +0800

    Queriers

--- x/nameservice/keeper/querier.go
+++ x/nameservice/keeper/querier.go
@@ -12,6 +12,14 @@ import (
 	"github.com/ilyabukalov/nameservice/x/nameservice/types"
 )
 
+
+// query endpoints supported by the nameservice Querier
+const (
+	QueryResolve = "resolve"
+	QueryWhois   = "whois"
+	QueryNames   = "names"
+)
+
 // NewQuerier creates a new querier for nameservice clients.
 func NewQuerier(k Keeper) sdk.Querier {
 	return func(ctx sdk.Context, path []string, req abci.RequestQuery) ([]byte, error) {
@@ -19,6 +27,12 @@ func NewQuerier(k Keeper) sdk.Querier {
 		case types.QueryParams:
 			return queryParams(ctx, k)
 			// TODO: Put the modules query routes
+		case QueryResolve:
+			return queryResolve(ctx, path[1:], req, keeper)
+		case QueryWhois:
+			return queryWhois(ctx, path[1:], req, keeper)
+		case QueryNames:
+			return queryNames(ctx, req, keeper)
 		default:
 			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, "unknown nameservice query endpoint")
 		}
@@ -36,5 +50,47 @@ func queryParams(ctx sdk.Context, k Keeper) ([]byte, error) {
 	return res, nil
 }
 
-// TODO: Add the modules query functions
-// They will be similar to the above one: queryParams()
+// nolint: unparam
+func queryResolve(ctx sdk.Context, path []string, req abci.RequestQuery, keeper Keeper) ([]byte, error) {
+	value := keeper.ResolveName(ctx, path[0])
+
+	if value == "" {
+		return []byte{}, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, "could not resolve name")
+	}
+
+	res, err := codec.MarshalJSONIndent(keeper.cdc, types.QueryResResolve{Value: value})
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+
+	return res, nil
+}
+
+// nolint: unparam
+func queryWhois(ctx sdk.Context, path []string, req abci.RequestQuery, keeper Keeper) ([]byte, error) {
+	whois := keeper.GetWhois(ctx, path[0])
+
+	res, err := codec.MarshalJSONIndent(keeper.cdc, whois)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+
+	return res, nil
+}
+
+func queryNames(ctx sdk.Context, req abci.RequestQuery, keeper Keeper) ([]byte, error) {
+	var namesList types.QueryResNames
+
+	iterator := keeper.GetNamesIterator(ctx)
+
+	for ; iterator.Valid(); iterator.Next() {
+		namesList = append(namesList, string(iterator.Key()))
+	}
+
+	res, err := codec.MarshalJSONIndent(keeper.cdc, namesList)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+
+	return res, nil
+}
